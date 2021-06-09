5542deea622787730ce0d7f1e2c5c86ba33a5074
Ciarm-nik <nikcia@libero.it>
2021-06-09 13:19:27 +0200

    Es 1 - Es 2

--- query.sql
+++ query.sql
@@ -1,25 +1,31 @@
--- 1.Selezionatuttigliospitichesonostatiidentificaticonlacartadiidentità
+-- 1.Seleziona tutti gli ospiti che sono stati identificati con la carta diidentità
+
+SELECT * FROM `ospiti` 
+WHERE `document_type` = "CI";
 
 
--- 2.Selezionatuttigliospitichesononatidopoil1988
+-- 2.Seleziona tutti gli ospiti che sono nati dopo il 1988
+
+SELECT * FROM `ospiti` 
+WHERE `date_of_birth` >= 1988;
 
 
--- 3.Selezionatuttigliospitichehannopiùdi20anni(almomentodell’esecuzionedellaquery)
+-- 3.Seleziona tutti gli ospiti che hanno più di 20anni(al momento dell’esecuzione della query)
 
 
--- 4.SelezionatuttigliospitiilcuinomeiniziaconlaD
+-- 4.Seleziona tutti gli ospiti il cui nome inizia con la D
 
 
--- 5.Calcolailtotaleincassatodegliordiniaccepted
+-- 5.Calcola il totale in cassato degli ordini accepted
 
 
--- 6.Qualèilprezzomassimopagato?
+-- 6.Qualè il prezz omassimo pagato?
 
 
--- 7.Selezionagliospitiriconosciuticonpatenteenatinel1975
+-- 7.Seleziona gli ospiti riconosciuti con patente e nati nel 1975
 
 
--- 8.Quantipagantisonoancheospiti?
+-- 8.Quanti paganti sono ancheo spiti?
 
 
--- 9.Quantipostilettohal’hotelintotale?
+-- 9.Quanti posti letto ha l’hotel in totale?
