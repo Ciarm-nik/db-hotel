dea5a6d348daaabea0436c72efb938146ba36985
Ciarm-nik <nikcia@libero.it>
2021-06-09 13:03:30 +0200

    Initial commit

---
+++ query.sql
@@ -0,0 +1,25 @@
+-- 1.Selezionatuttigliospitichesonostatiidentificaticonlacartadiidentità
+
+
+-- 2.Selezionatuttigliospitichesononatidopoil1988
+
+
+-- 3.Selezionatuttigliospitichehannopiùdi20anni(almomentodell’esecuzionedellaquery)
+
+
+-- 4.SelezionatuttigliospitiilcuinomeiniziaconlaD
+
+
+-- 5.Calcolailtotaleincassatodegliordiniaccepted
+
+
+-- 6.Qualèilprezzomassimopagato?
+
+
+-- 7.Selezionagliospitiriconosciuticonpatenteenatinel1975
+
+
+-- 8.Quantipagantisonoancheospiti?
+
+
+-- 9.Quantipostilettohal’hotelintotale?
